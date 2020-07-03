6fd188840135a3822ae02175ad6f726edbf3b08b
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-07-03 16:29:38 -0700

    Add barebone http server

--- svanill-vault-server/src/bin/svanill-vault-server.rs
+++ svanill-vault-server/src/bin/svanill-vault-server.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use std::env;
+
+#[get("/")]
+async fn index() -> impl Responder {
+    format!("todo")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
