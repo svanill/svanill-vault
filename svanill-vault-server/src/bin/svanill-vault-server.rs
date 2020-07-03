21e6b40dc1db02b41508eb47ebdee5f623c42b38
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-07-03 16:48:10 -0700

    Add options for host and port

--- svanill-vault-server/src/bin/svanill-vault-server.rs
+++ svanill-vault-server/src/bin/svanill-vault-server.rs
@@ -1,5 +1,20 @@
 use actix_web::{get, App, HttpServer, Responder};
 use std::env;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "svanill-vault-cli",
+    about = "Read/Write data from/to a svanill vault server"
+)]
+struct Opt {
+    /// Server host
+    #[structopt(short = "H", default_value = "localhost")]
+    host: String,
+    /// Server port
+    #[structopt(short = "P", default_value = "8080")]
+    port: u16,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -11,8 +26,10 @@ async fn main() -> std::io::Result<()> {
     env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
     env_logger::init();
 
+    let opt = Opt::from_args();
+
     HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:8080")?
+        .bind((opt.host, opt.port))?
         .run()
         .await
 }
