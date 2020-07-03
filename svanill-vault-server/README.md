adb85bb16c37f1cec7ca7dc3b75fdfe39577b3c0
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-07-02 23:24:32 -0700

    Add openapi model generation

--- svanill-vault-server/README.md
+++ svanill-vault-server/README.md
@@ -2,3 +2,17 @@
 
 An HTTP server to store/retrieve files produced by svanill ([cli](https://github.com/svanill/svanill-cli) or [web](https://github.com/svanill/svanill)) files.
 
+## Build
+
+You need to first generate some files from the openapi specifications.
+
+```
+./regenerate_models.sh
+```
+
+Then as usual
+
+```
+cargo build
+```
+
---
+++ svanill-vault-server/regenerate_models.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eux
+
+rm -fr tmp_build
+rm -fr src/models
+mkdir tmp_build
+cp openapi3.yml tmp_build/
+
+docker run -u $(id -u) --rm -v ${PWD}/tmp_build:/local \
+    openapitools/openapi-generator-cli \
+    generate \
+    -g rust \
+    --global-property=modelDocs=false \
+    -i /local/openapi3.yml \
+    -o /local/out/
+
+cp -R tmp_build/out/src/models/ src/models
+rm -fr tmp_build
