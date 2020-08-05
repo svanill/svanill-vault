8fe02c6f79d03ee7cc14ef600aadefcad60884ae
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-08-04 23:40:05 -0700

    Test challenge request without username

--- svanill-vault-server/tests/server.rs
+++ svanill-vault-server/tests/server.rs
@@ -1,11 +1,20 @@
 use actix_web::{http::Method, test, App};
 use ctor::ctor;
+use diesel::{
+    r2d2::{self, ConnectionManager},
+    SqliteConnection,
+};
+use r2d2::Pool;
 use std::sync::{Arc, RwLock};
 use svanill_vault_server::auth::auth_token::AuthToken;
 use svanill_vault_server::auth::tokens_cache::TokensCache;
 use svanill_vault_server::errors::ApiError;
 use svanill_vault_server::http::handlers::config_handlers;
 use svanill_vault_server::openapi_models::GetStartingEndpointsResponse;
+
+#[macro_use]
+extern crate diesel_migrations;
+embed_migrations!();
 
 #[cfg(test)]
 #[ctor]
@@ -81,4 +90,34 @@ async fn root() {
     let resp: GetStartingEndpointsResponse = test::read_response_json(&mut app, req).await;
 
     assert_eq!(200, resp.status);
+}
+
+fn setup_test_db() -> Pool<ConnectionManager<SqliteConnection>> {
+    let connspec = ":memory:";
+    let manager = ConnectionManager::<SqliteConnection>::new(connspec);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create database connection pool");
+
+    let connection = pool.get().expect("couldn't get db connection from pool");
+
+    embedded_migrations::run(&connection).expect("failed to run migrations");
+    pool
+}
+
+#[actix_rt::test]
+async fn get_auth_challenge_no_username_provided() {
+    let pool = setup_test_db();
+
+    let mut app =
+        test::init_service(App::new().data(pool.clone()).configure(config_handlers)).await;
+
+    let req = test::TestRequest::get()
+        .uri("/auth/request-challenge")
+        .to_request();
+
+    let resp: ApiError = test::read_response_json(&mut app, req).await;
+
+    assert_eq!(409, resp.http_status);
+    assert_eq!(1002, resp.error.code);
 }
