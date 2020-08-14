3cbb3b66b8ae24c25b6daf6fd2e31171b15750a2
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-08-13 23:59:01 -0700

    Refactor delete_files_ok to use real server

--- svanill-vault-server/tests/server.rs
+++ svanill-vault-server/tests/server.rs
@@ -652,22 +652,28 @@ async fn delete_files_ok() {
     let s3_resp_mock = MockRequestDispatcher::with_status(StatusCode::NO_CONTENT.as_u16());
     let s3_fs = setup_s3_fs(s3_resp_mock);
 
-    let mut app = test::init_service(
-        App::new()
-            .data(pool)
-            .data(Arc::new(s3_fs))
-            .data(Arc::new(RwLock::new(tokens_cache)))
-            .configure(config_handlers),
-    )
-    .await;
+    let address = spawn_app(
+        AppData::new()
+            .pool(pool)
+            .tokens_cache(tokens_cache)
+            .s3_fs(s3_fs),
+    );
 
-    let req = test::TestRequest::with_header("Authorization", "Bearer dummy-valid-token")
-        .method(Method::DELETE)
-        .uri("/files/?filename=same-wheter-it-exist-or-not")
-        .to_request();
+    let client = reqwest::Client::new();
+    let resp = client
+        .delete(&format!(
+            "{}/files/?filename=same-wheter-it-exist-or-not",
+            &address
+        ))
+        .header("Authorization", "Bearer dummy-valid-token")
+        .send()
+        .await
+        .expect("Failed to execute request");
 
-    let resp = app.call(req).await.expect("failed to make the request");
-    let json_resp: RemoveFileResponse = to_json_response(resp).await.unwrap();
+    let json_resp: RemoveFileResponse = resp
+        .json::<RemoveFileResponse>()
+        .await
+        .expect("Cannot decode JSON response");
 
     assert_eq!(200, json_resp.status);
 }
