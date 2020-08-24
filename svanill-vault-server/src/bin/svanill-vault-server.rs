fb4c79a9adb5ef1a2cba64bd95a4c20e9dcce454
Riccardo Attilio Galli <riccardo@sideralis.org>
2020-08-24 00:02:45 -0700

    Add Sentry support

--- svanill-vault-server/src/bin/svanill-vault-server.rs
+++ svanill-vault-server/src/bin/svanill-vault-server.rs
@@ -91,6 +91,17 @@ async fn main() -> Result<()> {
     #[cfg(debug_assertions)]
     color_backtrace::install();
 
+    if std::env::var_os("SENTRY_DSN").is_none() {
+        log::warn!("Env var SENTRY_DSN not set, Sentry won't be initialized");
+    }
+
+    // Note: requires env SENTRY_DSN to be properly set to become active
+    let _guard = sentry::init(sentry::ClientOptions {
+        release: Some(format!("svanill-vault-server@{}", std::env!("GIT_HASH")).into()),
+        attach_stacktrace: true,
+        ..Default::default()
+    });
+
     let opt = Opt::from_args();
 
     setup_log(if opt.verbose == 1 {
